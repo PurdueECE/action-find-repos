d5a5dd9f93b73925c7012028d353842f63d9e8a1
Geoffrey Cramer <gcwill70@gmail.com>
2022-04-08 17:31:02 -0400

    update more timestamps

--- test_unit/test_ece364prelabs.py
+++ test_unit/test_ece364prelabs.py
@@ -5,8 +5,8 @@ from unittest import mock
 @mock.patch.dict(os.environ, {
     "INPUT_ORG": "PurdueECE364",
     "INPUT_PATTERN": '^prelabs-.*$',
-    "INPUT_CREATED_AFTER": "2022-01-01T00:00:00",
-    "INPUT_CREATED_BEFORE": "2022-06-01T00:00:00"
+    "INPUT_CREATED_AFTER": "01/01/2022 00:00:00",
+    "INPUT_CREATED_BEFORE": "01/06/2022 00:00:00"
     })
 def test_ece364sp22prelabs():
     main()
